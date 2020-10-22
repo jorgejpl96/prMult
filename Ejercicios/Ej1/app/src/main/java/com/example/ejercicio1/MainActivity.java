1ae646fb8b605dc37566ca9e33b0119f22230b1e
jorgejpl96 <55388393+jorgejpl96@users.noreply.github.com>
2020-10-23 00:08:17 +0200

    .

--- Ejercicios/Ej1/app/src/main/java/com/example/ejercicio1/MainActivity.java
+++ Ejercicios/Ej1/app/src/main/java/com/example/ejercicio1/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.ejercicio1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button miBoton1=(Button)findViewById(R.id.boton1);
+
+        miBoton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity();
+            }
+        });
+
     }
 }
